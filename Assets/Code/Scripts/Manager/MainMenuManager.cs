466657c919d9f2bd91f17d23d4d2602edfaaf86d
Selin Mergen <selinmergen@gmail.com>
2023-01-31 19:00:25 +0300

    Fixed Bug on Missing Script
    Implemented opening closing menu by X button and added opening vr keyboard button

--- Assets/Code/Scripts/Manager/MainMenuManager.cs
+++ Assets/Code/Scripts/Manager/MainMenuManager.cs
@@ -1,17 +1,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class MainMenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public Button exitButton;
-    public int currentPanelNumber = 0;
-    public List<GameObject> panels = new List<GameObject>();
-    private List<Panel> panelHistory = new List<Panel>();
+    public int currentPanelNumber;
+    public List<GameObject> panels;
+    private bool isMenuActive;
+    
+    [SerializeField]
+    InputActionReference openCloseInput;
+    
+    [SerializeField]
+    GameObject menuGameObject;
 
     void Start()
     {
+        if (menuGameObject != null) 
+            menuGameObject.SetActive(isMenuActive);
+        
         SetupPanels();
         exitButton.onClick.AddListener((() => { Application.Quit(); }));
     }
@@ -23,5 +33,27 @@ public class MainMenuManager : MonoBehaviour
             panel.SetActive(false);
         }
         panels[currentPanelNumber].SetActive(true);
+    }
+    
+    private void OnEnable()
+    {
+        openCloseInput.action.performed += OpenCloseMenu;
+    }
+    
+    private void OnDisable()
+    {
+        openCloseInput.action.performed -= OpenCloseMenu;
+    }
+    
+    private void OpenCloseMenu(InputAction.CallbackContext obj)
+    {
+        if (!isMenuActive) {
+            isMenuActive = true;
+            menuGameObject.SetActive(true);
+        }
+        else {
+            isMenuActive = false;
+            menuGameObject.SetActive(false);
+        }
     }
 }
