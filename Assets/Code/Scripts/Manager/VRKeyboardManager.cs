6247839f0957166fe432450a4691bf4150435e84
Selin Mergen <selinmergen@gmail.com>
2023-02-04 20:06:35 +0300

    Fixed writing same text into all input fields bug.

--- Assets/Code/Scripts/Manager/VRKeyboardManager.cs
+++ Assets/Code/Scripts/Manager/VRKeyboardManager.cs
@@ -5,8 +5,7 @@ using System.Collections.Generic;
 using UnityEngine.XR.Interaction.Toolkit;
 using Keyboard = VRKeys.Keyboard;
 
-public class VRKeyboardManager : MonoBehaviour
-{
+public class VRKeyboardManager : MonoBehaviour {
     public Vector3 relativePosition = new Vector3(0f, 0f, 2f);
     public GameObject playerCamera;
     public Keyboard keyboard;
@@ -15,81 +14,75 @@ public class VRKeyboardManager : MonoBehaviour
     public GameObject rightHandController;
     public GameObject leftMallet;
     public GameObject rightMallet;
-    
+
     private int _currentInputFieldNumber;
-    public void EnableVRKeyboard(int inputFieldNumber)
-	{
-		_currentInputFieldNumber = inputFieldNumber;
-		keyboard.Enable();
-		keyboard.canvas.gameObject.SetActive(false);
-		keyboard.OnUpdate.AddListener(HandleUpdate);
-		keyboard.OnSubmit.AddListener(HandleSubmit);
-		keyboard.OnCancel.AddListener(HandleCancel);
 
-		keyboard.gameObject.transform.position = playerCamera.transform.position + relativePosition;
-		AttachMallets();
+    public void EnableVRKeyboard(int inputFieldNumber) {
+        _currentInputFieldNumber = inputFieldNumber;
+        keyboard.Enable();
+        keyboard.canvas.gameObject.SetActive(false);
+        keyboard.OnUpdate.AddListener(HandleUpdate);
+        keyboard.OnSubmit.AddListener(HandleSubmit);
+        keyboard.OnCancel.AddListener(HandleCancel);
 
-		leftHandController.GetComponent<XRRayInteractor>().enabled = false;
-		rightHandController.GetComponent<XRRayInteractor>().enabled = false;
-	}
+        keyboard.gameObject.transform.position = playerCamera.transform.position + relativePosition;
+        AttachMallets();
 
-	private void AttachMallets()
-	{
-		leftMallet.transform.SetParent(leftHandController.transform);
-		leftMallet.transform.localPosition = Vector3.zero;
-		leftMallet.transform.localRotation = Quaternion.Euler(new Vector3(90f,0f,0f));
-		leftMallet.SetActive(true);
+        leftHandController.GetComponent<XRRayInteractor>().enabled = false;
+        rightHandController.GetComponent<XRRayInteractor>().enabled = false;
+    }
 
-		rightMallet.transform.SetParent(rightHandController.transform);
-		rightMallet.transform.localPosition = Vector3.zero;
-		rightMallet.transform.localRotation = Quaternion.Euler(new Vector3(90f, 0f, 0f));
-		rightMallet.SetActive(true);
-	}
+    private void AttachMallets() {
+        leftMallet.transform.SetParent(leftHandController.transform);
+        leftMallet.transform.localPosition = Vector3.zero;
+        leftMallet.transform.localRotation = Quaternion.Euler(new Vector3(90f, 0f, 0f));
+        leftMallet.SetActive(true);
 
-	private void DetachMallets()
-	{
-		leftMallet.transform.SetParent(null);
-		leftMallet.SetActive(false);
+        rightMallet.transform.SetParent(rightHandController.transform);
+        rightMallet.transform.localPosition = Vector3.zero;
+        rightMallet.transform.localRotation = Quaternion.Euler(new Vector3(90f, 0f, 0f));
+        rightMallet.SetActive(true);
+    }
 
-		rightMallet.transform.SetParent(null);
-		rightMallet.SetActive(false);
-	}
+    private void DetachMallets() {
+        leftMallet.transform.SetParent(null);
+        leftMallet.SetActive(false);
 
-	private void DisableVRKeyboard() 
-	{
-		keyboard.OnUpdate.RemoveListener(HandleUpdate);
-		keyboard.OnSubmit.RemoveListener(HandleSubmit);
-		keyboard.OnCancel.RemoveListener(HandleCancel);
+        rightMallet.transform.SetParent(null);
+        rightMallet.SetActive(false);
+    }
 
-		keyboard.Disable();
+    private void DisableVRKeyboard() {
+        keyboard.OnUpdate.RemoveListener(HandleUpdate);
+        keyboard.OnSubmit.RemoveListener(HandleSubmit);
+        keyboard.OnCancel.RemoveListener(HandleCancel);
 
-		DetachMallets();
+        keyboard.Disable();
 
-		leftHandController.GetComponent<XRRayInteractor>().enabled = true;
-		rightHandController.GetComponent<XRRayInteractor>().enabled = true;
-	}
+        DetachMallets();
 
-	private void HandleUpdate(string text)
-	{
-		keyboard.HideValidationMessage();
-		var inputField = inputFields[_currentInputFieldNumber];
-		inputField.text = text;
-		inputField.caretPosition = inputField.text.Length;
-	}
-	
-	private void HandleSubmit(string text)
-	{
-		DisableVRKeyboard();
+        leftHandController.GetComponent<XRRayInteractor>().enabled = true;
+        rightHandController.GetComponent<XRRayInteractor>().enabled = true;
+    }
 
-		var eventSystem = EventSystem.current;
-		if (!eventSystem.alreadySelecting) eventSystem.SetSelectedGameObject(null);
-	}
+    private void HandleUpdate(string text) {
+        keyboard.HideValidationMessage();
+        var inputField = inputFields[_currentInputFieldNumber];
+        inputField.text += text.Substring(text.Length - 1);
+        inputField.caretPosition = inputField.text.Length;
+    }
 
-	private void HandleCancel()
-	{
-		DisableVRKeyboard();
+    private void HandleSubmit(string text) {
+        DisableVRKeyboard();
 
-		var eventSystem = EventSystem.current;
-		if (!eventSystem.alreadySelecting) eventSystem.SetSelectedGameObject(null);
-	}
+        var eventSystem = EventSystem.current;
+        if (!eventSystem.alreadySelecting) eventSystem.SetSelectedGameObject(null);
+    }
+
+    private void HandleCancel() {
+        DisableVRKeyboard();
+
+        var eventSystem = EventSystem.current;
+        if (!eventSystem.alreadySelecting) eventSystem.SetSelectedGameObject(null);
+    }
 }
