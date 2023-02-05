285300ad3b8e58482996a91cd0bc6da60c96b2c4
Selin Mergen <selinmergen@gmail.com>
2023-02-05 03:10:13 +0300

    Basics of Multiplayer game designs have been completed. Now a user can easily list the servers, join with name or create a room. Changing region or joining room from the list will be implemented as well.

--- Assets/Code/Scripts/Manager/RoomManager.cs
+++ Assets/Code/Scripts/Manager/RoomManager.cs
@@ -1,17 +1,84 @@
 using TMPro;
-using UnityEngine;
 using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using System.Collections.Generic;
 
-public class RoomManager : MonoBehaviour
+public class RoomManager : MonoBehaviourPunCallbacks
 {
+    // Create Room
     [SerializeField]
-    private TMP_InputField _roomNameInputField;
+    private TMP_InputField newRoomNameInputField;
+    [SerializeField]
+    private TMP_Dropdown maxPlayersDropdown;
+    [SerializeField]
+    private TMP_Text errorText;
+    [SerializeField]
+    private Toggle isPrivateToggle;
+
+    // Join Room
+    [SerializeField]
+    private TMP_InputField joinRoomNameInputField;
     
-    public void CreateRoom()
-    {
-        if (_roomNameInputField.text != "" && PhotonNetwork.IsConnected)
-        {
-            Debug.Log("Create room with name: " + _roomNameInputField.text);
+    // Room List
+    [SerializeField] 
+    private RoomElement roomElementPrefab;
+    [SerializeField] 
+    private Transform content;
+
+    private List<RoomElement> _roomElements;
+    
+    public void ConnectServer() {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void  OnConnected() {
+        errorText.text = "Connected to server";
+    }
+
+    public void CreateRoom() {
+        if (newRoomNameInputField.text == "" || !PhotonNetwork.IsConnected) return;
+        RoomOptions roomOptions = new RoomOptions {
+            MaxPlayers = byte.Parse(maxPlayersDropdown.options[maxPlayersDropdown.value].text),
+            IsVisible = !isPrivateToggle.isOn
+        };
+        PhotonNetwork.JoinOrCreateRoom(newRoomNameInputField.text, roomOptions, TypedLobby.Default);
+        PhotonNetwork.JoinRoom(newRoomNameInputField.text);
+        PhotonNetwork.LoadLevel("Lobby Scene");
+    }
+
+    public void JoinRoom() {
+        if (joinRoomNameInputField.text == "" || !PhotonNetwork.IsConnected) return;
+        errorText.text = "Joining room...";
+        PhotonNetwork.JoinRoom(joinRoomNameInputField.text);
+        PhotonNetwork.LoadLevel("Lobby Scene");
+    }
+    
+    public string GetRoomInfo() {
+        Room currentRoom = PhotonNetwork.CurrentRoom;
+        return "Room name: " + currentRoom.Name + "\n" +
+                             "Players: " + currentRoom.PlayerCount + "/" + currentRoom.MaxPlayers;
+    }
+
+    public void DisconnectServer() {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList) {
+        foreach (var room in roomList) {
+            if (room.RemovedFromList) {
+                var index = _roomElements.FindIndex(x => x.RoomInfo.Name == room.Name);
+                if (index == -1) continue;
+                Destroy(_roomElements[index].gameObject);
+                _roomElements.RemoveAt(index);
+            }
+            else {
+                var roomElement = Instantiate(roomElementPrefab, content);
+                if (roomElement == null) continue;
+                roomElement.SetRoomInfo(room);
+                _roomElements.Add(roomElement);
+            }
         }
     }
 }
