2e68990cd5a776d1889c6a97c30924c8585ca1aa
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-09 17:09:19 +0300

    Added tracking camera feature for menu

--- Assets/Code/Scripts/Manager/MainMenuManager.cs
+++ Assets/Code/Scripts/Manager/MainMenuManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 using System.Collections.Generic;
 
 public class MainMenuManager : MonoBehaviour {
+
     public List<GameObject> panels;
 
     [SerializeField] private InputActionReference openCloseInput;
@@ -11,38 +12,34 @@ public class MainMenuManager : MonoBehaviour {
     [SerializeField] private float menuLerpSpeed = 0.05f;
     [SerializeField] private float menuDistance = 2f;
     [SerializeField] private VRKeyboardManager keyboardManager;
-    [SerializeField] private GameObject avatarSelectionPlatform;
-
+    
     private int _currentPanelNumber;
     private bool _isMenuActive;
-    private bool _isAvatarSelectionPlatformActive;
 
-    private void Start() {
+    void Start() {
         mainMenuPanel.SetActive(_isMenuActive);
-        avatarSelectionPlatform.SetActive(_isAvatarSelectionPlatformActive);
         SetupPanels();
     }
 
-    private void FixedUpdate() {
-        if (!_isMenuActive || keyboardManager.IsKeyboardActive() || _isAvatarSelectionPlatformActive) return;
-
+    private void Update() {
+        if (!_isMenuActive || keyboardManager.IsKeyboardActive()) return;
+        
         var mainMenuPosition = mainMenuPanel.transform.position;
-        mainMenuPosition = Vector3.Lerp(mainMenuPosition,
-            playerCamera.transform.position + playerCamera.transform.forward * menuDistance, menuLerpSpeed);
-        mainMenuPanel.transform.rotation = Quaternion.Lerp(mainMenuPanel.transform.rotation,
-            playerCamera.transform.rotation, menuLerpSpeed);
+        mainMenuPosition = Vector3.Lerp(mainMenuPosition, playerCamera.transform.position + playerCamera.transform.forward * menuDistance, menuLerpSpeed);
+        mainMenuPanel.transform.rotation = Quaternion.Lerp(mainMenuPanel.transform.rotation, playerCamera.transform.rotation, menuLerpSpeed);
         mainMenuPosition.y = mainMenuPosition.y < 0.5f ? 0.5f : mainMenuPosition.y;
         mainMenuPanel.transform.position = mainMenuPosition;
     }
 
     private void SetupPanels() {
-        foreach (var panel in panels) panel.SetActive(false);
+        foreach (var panel in panels) {
+            panel.SetActive(false);
+        }
 
         panels[_currentPanelNumber].SetActive(true);
     }
 
     public void GoToPanel(int newPanelNumber) {
-        if (keyboardManager.IsKeyboardActive()) keyboardManager.DisableVRKeyboard();
         panels[_currentPanelNumber].SetActive(false);
         _currentPanelNumber = newPanelNumber;
         panels[_currentPanelNumber].SetActive(true);
@@ -63,30 +60,16 @@ public class MainMenuManager : MonoBehaviour {
     private void OpenCloseMenu(InputAction.CallbackContext obj) {
         if (!_isMenuActive) {
             _isMenuActive = true;
-            mainMenuPanel.transform.position =
-                playerCamera.transform.position + playerCamera.transform.forward * menuDistance;
+            mainMenuPanel.transform.position = playerCamera.transform.position + playerCamera.transform.forward * menuDistance;
             var rotationVector = mainMenuPanel.transform.rotation.eulerAngles;
             rotationVector.y = playerCamera.transform.rotation.eulerAngles.y;
             mainMenuPanel.transform.rotation = Quaternion.Euler(rotationVector);
             mainMenuPanel.SetActive(true);
-            avatarSelectionPlatform.SetActive(_isAvatarSelectionPlatformActive);
         }
         else {
             _isMenuActive = false;
             mainMenuPanel.SetActive(false);
             keyboardManager.EmptyKeyboardInputField();
-            keyboardManager.DisableVRKeyboard();
-            avatarSelectionPlatform.SetActive(false);
         }
-    }
-
-    public void EnableAvatarSelectionPlatform() {
-        _isAvatarSelectionPlatformActive = true;
-        avatarSelectionPlatform.SetActive(true);
-    }
-
-    public void DisableAvatarSelectionPlatform() {
-        _isAvatarSelectionPlatformActive = false;
-        avatarSelectionPlatform.SetActive(false);
     }
 }
--- Assets/Code/Scripts/Manager/RoomManager.cs
+++ Assets/Code/Scripts/Manager/RoomManager.cs
@@ -4,156 +4,70 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Realtime;
 using System.Collections.Generic;
-using System.Linq;
-using ExitGames.Client.Photon;
 
-public class RoomManager : MonoBehaviourPunCallbacks {
-    // Nickname
-    [SerializeField] private TMP_InputField nicknameInputField;
-
+public class RoomManager : MonoBehaviourPunCallbacks
+{
     // Create Room
-    [SerializeField] private TMP_InputField newRoomNameInputField;
-    [SerializeField] private TMP_Dropdown maxPlayersDropdown;
-    [SerializeField] private Toggle isPrivateToggle;
-
-    [SerializeField] private TMP_Dropdown regionDropdownCreateMenu;
-    // [SerializeField]
-    // private TMP_InputField passwordInputField;
+    [SerializeField]
+    private TMP_InputField newRoomNameInputField;
+    [SerializeField]
+    private TMP_Dropdown maxPlayersDropdown;
+    [SerializeField]
+    private Toggle isPrivateToggle;
 
     // Join Room
-    [SerializeField] private TMP_Dropdown regionDropdownJoinPrivateMenu;
-    [SerializeField] private TMP_InputField joinRoomNameInputField;
-
+    [SerializeField]
+    private TMP_InputField joinRoomNameInputField;
+    
     // Room List
-    [SerializeField] private RoomElement roomElementPrefab;
-    [SerializeField] private Transform content;
-    [SerializeField] private TMP_Dropdown regionDropdownJoinPublicMenu;
+    [SerializeField] 
+    private RoomElement roomElementPrefab;
+    [SerializeField] 
+    private Transform content;
 
-    private Dictionary<string, RoomElement> _cachedRoomList = new();
-    private RoomInfo _selectedRoomInfo;
-
-    private void Start() {
-        regionDropdownCreateMenu.onValueChanged.AddListener(delegate { SetRegion(regionDropdownCreateMenu); });
-        regionDropdownJoinPrivateMenu.onValueChanged.AddListener(delegate {
-            SetRegion(regionDropdownJoinPrivateMenu);
-        });
-        regionDropdownJoinPublicMenu.onValueChanged.AddListener(delegate {
-            SetAndRefreshRegion(regionDropdownJoinPublicMenu);
-        });
-    }
-
+    private List<RoomElement> _roomElements;
+    
     public void ConnectServer() {
-        if (PhotonNetwork.IsConnectedAndReady) return;
-        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = "eu";
-        PhotonNetwork.ConnectUsingSettings();
+        if(!PhotonNetwork.IsConnected)
+            PhotonNetwork.ConnectUsingSettings();
+    }
+    
+
+    public void CreateRoom() {
+        if (newRoomNameInputField.text == "" || !PhotonNetwork.IsConnected) return;
+        RoomOptions roomOptions = new RoomOptions {
+            MaxPlayers = byte.Parse(maxPlayersDropdown.options[maxPlayersDropdown.value].text),
+            IsVisible = !isPrivateToggle.isOn
+        };
+        PhotonNetwork.JoinOrCreateRoom(newRoomNameInputField.text, roomOptions, TypedLobby.Default);
+        PhotonNetwork.JoinRoom(newRoomNameInputField.text);
+        PhotonNetwork.LoadLevel("Lobby Scene");
     }
 
-    private void Connect() {
-        if (PhotonNetwork.IsConnectedAndReady) return;
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    public override void OnConnectedToMaster() {
-        if (PhotonNetwork.InLobby) return;
-        PhotonNetwork.JoinLobby();
-        PhotonNetwork.AutomaticallySyncScene = true;
-    }
-
-    public override void OnJoinedLobby() {
-        PhotonNetwork.NickName = "Player" + Random.Range(0, 1000);
+    public void JoinRoom() {
+        if (joinRoomNameInputField.text == "" || !PhotonNetwork.IsConnected) return;
+        PhotonNetwork.JoinRoom(joinRoomNameInputField.text);
+        PhotonNetwork.LoadLevel("Lobby Scene");
     }
 
     public void DisconnectServer() {
         PhotonNetwork.Disconnect();
     }
 
-    public void SetNickname() {
-        PhotonNetwork.NickName = nicknameInputField.text;
-    }
-
-    public void CreateRoom() {
-        if (newRoomNameInputField.text == "") return;
-        if (!PhotonNetwork.IsConnected) Connect();
-        var customRoomProperties = new Hashtable {
-            { "Region", regionDropdownCreateMenu.options[regionDropdownCreateMenu.value].text }
-        };
-        // if (passwordInputField.text != "" && isPrivateToggle.isOn) {
-        //     customRoomProperties.Add("Password", passwordInputField.text);
-        // }
-        var roomOptions = new RoomOptions {
-            MaxPlayers = byte.Parse(maxPlayersDropdown.options[maxPlayersDropdown.value].text),
-            IsVisible = isPrivateToggle.isOn,
-            CustomRoomProperties = customRoomProperties
-        };
-
-        PhotonNetwork.JoinOrCreateRoom(newRoomNameInputField.text, roomOptions, TypedLobby.Default);
-        PhotonNetwork.LoadLevel("Lobby Scene");
-    }
-
-    public void JoinRoomWithName() {
-        if (joinRoomNameInputField.text == "") return;
-        if (!PhotonNetwork.IsConnected) Connect();
-        PhotonNetwork.JoinRoom(joinRoomNameInputField.text);
-        PhotonNetwork.LoadLevel("Lobby Scene");
-    }
-
-    public void OnClick_Join() {
-        if (_selectedRoomInfo == null) return;
-        if (!PhotonNetwork.IsConnected) Connect();
-        PhotonNetwork.JoinRoom(_selectedRoomInfo.Name);
-        PhotonNetwork.LoadLevel("Lobby Scene");
-    }
-
-    public void OnClick_RoomElement(RoomInfo roomInfo) {
-        _selectedRoomInfo = roomInfo;
-    }
-
     public override void OnRoomListUpdate(List<RoomInfo> roomList) {
-        base.OnRoomListUpdate(roomList);
-        UpdateCachedRoomList(roomList);
-    }
-
-    private void UpdateCachedRoomList(List<RoomInfo> roomList) {
-        foreach (var roomInfo in roomList)
-            switch (_cachedRoomList.Count) {
-                case > 0 when roomInfo.RemovedFromList:
-                    Destroy(_cachedRoomList[roomInfo.Name].gameObject);
-                    _cachedRoomList.Remove(roomInfo.Name);
-                    break;
-                case > 0 when _cachedRoomList.ContainsKey(roomInfo.Name):
-                    _cachedRoomList[roomInfo.Name].SetRoomInfo(roomInfo);
-                    break;
-                default: {
-                    if (roomInfo.MaxPlayers == 0)
-                        break;
-                    var roomElement = Instantiate(roomElementPrefab, content);
-                    if (roomElement == null) continue;
-                    roomElement.SetRoomInfo(roomInfo);
-                    roomElement.SetRoomManager(this);
-                    _cachedRoomList.Add(roomInfo.Name, roomElement);
-                    break;
-                }
+        foreach (var room in roomList) {
+            if (room.RemovedFromList) {
+                var index = _roomElements.FindIndex(x => x.RoomInfo.Name == room.Name);
+                if (index == -1) continue;
+                Destroy(_roomElements[index].gameObject);
+                _roomElements.RemoveAt(index);
             }
-    }
-
-    public void ClearRoomList() {
-        foreach (var roomElement in _cachedRoomList.Values) Destroy(roomElement.gameObject);
-        _cachedRoomList.Clear();
-    }
-
-    private static void SetRegion(TMP_Dropdown region) {
-        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = region.options[region.value].text;
-        PhotonNetwork.Disconnect();
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    private void SetAndRefreshRegion(TMP_Dropdown region) {
-        SetRegion(region);
-        var currentRegion = region.options[region.value].text;
-        foreach (var roomElement in _cachedRoomList.Values.Where(roomElement =>
-                     currentRegion != roomElement.GetRoomRegion())) {
-            Destroy(roomElement.gameObject);
-            _cachedRoomList.Remove(roomElement.GetRoomInfo().Name);
+            else {
+                var roomElement = Instantiate(roomElementPrefab, content);
+                if (roomElement == null) continue;
+                roomElement.SetRoomInfo(room);
+                _roomElements.Add(roomElement);
+            }
         }
     }
 }
