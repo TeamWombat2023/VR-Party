92566bb97657d3e84356ce566b767b70cacf3e92
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-10 20:18:13 +0300

    Added Necessary functions for RoomManager

--- Assets/Code/Scripts/Manager/RoomElement.cs
+++ Assets/Code/Scripts/Manager/RoomElement.cs
@@ -5,37 +5,12 @@ using Photon.Realtime;
 public class RoomElement : MonoBehaviour {
     
     [SerializeField]
-    private TMP_Text roomNameText;
-    [SerializeField]
-    private TMP_Text roomPlayerCountText;
-    [SerializeField]
-    private TMP_Text roomRegionText;
-
-    private RoomManager RoomManager { get; set; }
+    private TMP_Text roomInfoText;
+  
     private RoomInfo RoomInfo { get; set; }
-    private string RoomRegion { get; set; }
 
     public void SetRoomInfo(RoomInfo roomInfo) {
         RoomInfo = roomInfo;
-        roomNameText.text = roomInfo.Name;
-        roomPlayerCountText.text = roomInfo.PlayerCount + "/" + roomInfo.MaxPlayers;
-        SetRoomRegion(roomInfo.CustomProperties.ContainsKey("Region") ? roomInfo.CustomProperties["Region"].ToString() : "EU");
-        roomRegionText.text = RoomRegion;
-    }
-    public RoomInfo GetRoomInfo() {
-        return RoomInfo;
-    }
-    public void SetRoomManager(RoomManager roomManager) {
-        RoomManager = roomManager;
-    }
-    private void SetRoomRegion(string roomRegion) {
-        RoomRegion = roomRegion;
-    }
-    public string GetRoomRegion() {
-        return RoomRegion;
-    }
-    
-    public void OnClickButton() {
-        RoomManager.OnClick_RoomElement(RoomInfo);
-    }
+        roomInfoText.text = roomInfo.MaxPlayers + " " + roomInfo.Name;
+    }    
 }
