28fd61e229773688a3194637d06c34275fb79f29
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-13 22:38:45 +0300

    Added changing region functionality.
    Added 2 dropdowns under Join Menu and Create Room Menu.
    Listing rooms content change to a simple table view.
    Added proper header.
    XR Interaction Toolkit folder moved under Imports.

--- Assets/Code/Scripts/Manager/RoomElement.cs
+++ Assets/Code/Scripts/Manager/RoomElement.cs
@@ -5,20 +5,25 @@ using Photon.Realtime;
 public class RoomElement : MonoBehaviour {
     
     [SerializeField]
-    private TMP_Text roomInfoText;
+    private TMP_Text roomNameText;
+    [SerializeField]
+    private TMP_Text roomPlayerCountText;
+    [SerializeField]
+    private TMP_Text roomRegionText;
 
     private RoomManager RoomManager { get; set; }
     private RoomInfo RoomInfo { get; set; }
 
     public void SetRoomInfo(RoomInfo roomInfo) {
         RoomInfo = roomInfo;
-        roomInfoText.text = roomInfo.MaxPlayers + " " + roomInfo.Name;
+        roomNameText.text = roomInfo.Name;
+        roomPlayerCountText.text = roomInfo.PlayerCount + "/" + roomInfo.MaxPlayers;
+        roomRegionText.text = roomInfo.CustomProperties.ContainsKey("Region") ? roomInfo.CustomProperties["Region"].ToString() : "Unknown";
     }
     public void SetRoomManager(RoomManager roomManager) {
         RoomManager = roomManager;
     }
     public void OnClickButton() {
-        Debug.Log("OnClick");
         RoomManager.OnClick_RoomElement(RoomInfo);
     }
 }
