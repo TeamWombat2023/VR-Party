f1159fd7e9f08d4ea50ff9dad6f1c09e270bcf3f
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-13 23:20:08 +0300

    Added refresh button functionality and necessary functions

--- Assets/Code/Scripts/Manager/RoomElement.cs
+++ Assets/Code/Scripts/Manager/RoomElement.cs
@@ -13,16 +13,28 @@ public class RoomElement : MonoBehaviour {
 
     private RoomManager RoomManager { get; set; }
     private RoomInfo RoomInfo { get; set; }
+    private string RoomRegion { get; set; }
 
     public void SetRoomInfo(RoomInfo roomInfo) {
         RoomInfo = roomInfo;
         roomNameText.text = roomInfo.Name;
         roomPlayerCountText.text = roomInfo.PlayerCount + "/" + roomInfo.MaxPlayers;
-        roomRegionText.text = roomInfo.CustomProperties.ContainsKey("Region") ? roomInfo.CustomProperties["Region"].ToString() : "Unknown";
+        SetRoomRegion(roomInfo.CustomProperties.ContainsKey("Region") ? roomInfo.CustomProperties["Region"].ToString() : "EU");
+        roomRegionText.text = RoomRegion;
+    }
+    public RoomInfo GetRoomInfo() {
+        return RoomInfo;
     }
     public void SetRoomManager(RoomManager roomManager) {
         RoomManager = roomManager;
     }
+    private void SetRoomRegion(string roomRegion) {
+        RoomRegion = roomRegion;
+    }
+    public string GetRoomRegion() {
+        return RoomRegion;
+    }
+    
     public void OnClickButton() {
         RoomManager.OnClick_RoomElement(RoomInfo);
     }
