23f9665b0e9222bd739cf2354b005ff772430908
Ömer Kaan Gürbüz <okg21@pop-os.localdomain>
2023-02-16 16:18:02 +0300

    Added unit scores, new tags for collisions with objects

--- Assets/Code/Scripts/Labyrinth/PlayerBehaviour.cs
+++ Assets/Code/Scripts/Labyrinth/PlayerBehaviour.cs
@@ -28,6 +28,19 @@ public class PlayerBehaviour : MonoBehaviour
         Debug.Log(GameManager.gameManager._playerHealth.currentHealth);
     }
 
+    private void IncreaseScore(int score)
+    {
+        GameManager.gameManager._playerScore.IncreaseScore(score);
+        Debug.Log(GameManager.gameManager._playerScore.currentScore);
+    }
+
+    private void DecreaseScore(int score)
+    {
+        GameManager.gameManager._playerScore.DecreaseScore(score);
+        Debug.Log(GameManager.gameManager._playerScore.currentScore);
+    }
+
+    //actions on collision
     void OnTriggerEnter(Collider other) 
     {
         //if heal object
@@ -35,12 +48,24 @@ public class PlayerBehaviour : MonoBehaviour
         {
             PlayerHeal(10);
         }
+
         //if damage object
         else if (other.gameObject.CompareTag ("DamagePickup"))
         {
             PlayerTakeDmg(10);
         }
+        
         //if score object
+        else if (other.gameObject.CompareTag ("ScorePickup"))
+        {
+            IncreaseScore(10);
+        }
+
+        //if trap object
+        else if (other.gameObject.CompareTag ("TrapPickup"))
+        {
+            DecreaseScore(10);
+        }
 
         //destroy after pickup
         other.gameObject.SetActive (false);
