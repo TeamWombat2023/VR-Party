de5226dcd681f28caf5481b7e79c9631283ddf68
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-16 15:36:33 +0300

    Added Avatar and Mirror assets

---
+++ Assets/Code/Scripts/Avatar/AvatarHolder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AvatarHolder : MonoBehaviour {
+
+    public Transform mainAvatarTransform;
+    public Transform headTransform;
+    public Transform bodyTransform;
+    public Transform handLeftTransform;
+    public Transform handRightTransform;
+
+    private void Start() {
+        SetLayerRecursively(headTransform.gameObject, 10);
+        SetLayerRecursively(bodyTransform.gameObject, 11);
+    }
+
+    private static void SetLayerRecursively(GameObject go, int layerNumber) {
+        if (go == null) return;
+        foreach (var trans in go.GetComponentsInChildren<Transform>(true)) {
+            trans.gameObject.layer = layerNumber;
+        }
+    }
+}
