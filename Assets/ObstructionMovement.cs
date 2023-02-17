d5aa33c0f0d9d93bead397b28cca414d5a2b78aa
kaanefekeles <kaanefek@gmail.com>
2023-02-17 16:17:44 +0300

    Obstruction movement

---
+++ Assets/ObstructionMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstructionMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    void Start()
+    {
+        //rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(-Vector3.right * Time.deltaTime * 10);
+        //rb.AddForce(2000 * Time.deltaTime, 0, 0);
+        //1rb.velocity = new Vector3(10, 0, 0);
+    }
+}
