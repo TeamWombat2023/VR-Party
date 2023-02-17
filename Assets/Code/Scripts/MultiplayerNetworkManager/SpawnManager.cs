49e10aeed81956260cb890b7a1ebbd94a763e744
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-17 16:48:48 +0300

    Added XR Origin to lobby

--- Assets/Code/Scripts/MultiplayerNetworkManager/SpawnManager.cs
+++ Assets/Code/Scripts/MultiplayerNetworkManager/SpawnManager.cs
@@ -6,11 +6,13 @@ public class SpawnManager : MonoBehaviour{
     [SerializeField]
     private GameObject genericPlayerPrefab;
     public Vector3 spawnPosition = new Vector3(0, 0, 0);
+    [SerializeField]
+    private GameObject xrOrigin;
     private void Start() {
         Invoke("SpawnPlayer", 1);
     }
     private void SpawnPlayer() {
-        if (!PhotonNetwork.IsConnectedAndReady) return;
-        PhotonNetwork.Instantiate(genericPlayerPrefab.name, spawnPosition, Quaternion.identity);
+        xrOrigin.SetActive(false);
+        genericPlayerPrefab = PhotonNetwork.Instantiate(genericPlayerPrefab.name, spawnPosition, Quaternion.identity);
     }
 }
