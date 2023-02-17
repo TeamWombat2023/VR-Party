1ae018c51a7eb878bae6421798719c9364dfbc86
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-17 15:24:04 +0300

    Added Spawning players into lobby feature

--- Assets/Code/Scripts/Avatar/AvatarSelectionPlatform.cs
+++ Assets/Code/Scripts/Avatar/AvatarSelectionPlatform.cs
@@ -1,31 +1,30 @@
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class AvatarSelectionPlatform : MonoBehaviour {
-    public List<GameObject> avatars = new();
+    
+    public List<GameObject> avatars = new List<GameObject>();
     public int currentAvatar = 0;
-
-    public AvatarSelectionManager avatarSelectionManager;
-
     private void Start() {
-        foreach (var avatar in avatars) avatar.SetActive(false);
+        foreach (var avatar in avatars) {
+            avatar.SetActive(false);
+        }
         avatars[currentAvatar].SetActive(true);
     }
-
+    
     public void NextAvatar() {
         avatars[currentAvatar].SetActive(false);
         currentAvatar = (currentAvatar + 1) % avatars.Count;
         avatars[currentAvatar].SetActive(true);
     }
-
+    
     public void PreviousAvatar() {
         avatars[currentAvatar].SetActive(false);
         currentAvatar = (currentAvatar - 1) % avatars.Count;
-        if (currentAvatar < 0) currentAvatar = avatars.Count - 1;
+        if (currentAvatar < 0) {
+            currentAvatar = avatars.Count - 1;
+        }
         avatars[currentAvatar].SetActive(true);
-    }
-
-    public void SelectAvatar() {
-        avatarSelectionManager.SelectAvatar(currentAvatar);
     }
 }
