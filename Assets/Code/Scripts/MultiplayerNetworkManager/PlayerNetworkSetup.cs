858bbf44e23c6e88d05d563a6154e5d01277f4a9
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-20 15:58:21 +0300

    Found the problem of moving after instantiation

--- Assets/Code/Scripts/MultiplayerNetworkManager/PlayerNetworkSetup.cs
+++ Assets/Code/Scripts/MultiplayerNetworkManager/PlayerNetworkSetup.cs
@@ -9,14 +9,14 @@ public class PlayerNetworkSetup : MonoBehaviourPunCallbacks {
     private void Start() {
         if(photonView.IsMine) {
             // This is my player
-            localXROrigin.SetActive(true);
             SetLayerRecursively(avatarHead, LayerMask.NameToLayer("LocalAvatarHead"));
             SetLayerRecursively(avatarBody, LayerMask.NameToLayer("LocalAvatarBody"));
+            localXROrigin.SetActive(true);
         } else {
             // This is another player
-            localXROrigin.SetActive(false);
             SetLayerRecursively(avatarHead, LayerMask.NameToLayer("Default"));
             SetLayerRecursively(avatarBody, LayerMask.NameToLayer("Default"));
+            localXROrigin.SetActive(false);
         }
     }
     private void SetLayerRecursively(GameObject obj, int newLayer) {
