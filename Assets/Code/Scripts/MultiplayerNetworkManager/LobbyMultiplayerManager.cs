b5a00d0fd69d615d62a645eeca6265a79d75ae46
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-20 15:19:01 +0300

    removed spawn manager

--- Assets/Code/Scripts/MultiplayerNetworkManager/LobbyMultiplayerManager.cs
+++ Assets/Code/Scripts/MultiplayerNetworkManager/LobbyMultiplayerManager.cs
@@ -8,11 +8,20 @@ public class LobbyMultiplayerManager : MonoBehaviourPunCallbacks
     public TMP_Text playerNamesText;
     public TMP_Text lobbyInfoText;
     public GameObject startTheGameButton;
+    
+    [SerializeField]
+    private GameObject xrOrigin;
+    [SerializeField]
+    private GameObject genericVRPlayerPrefab;
+    [SerializeField]
+    private Vector3 spawnPosition;
 
     public override void OnJoinedRoom() {
         ShowPlayers();
         WriteLobbyInformation(PhotonNetwork.CurrentRoom);
         startTheGameButton.SetActive(PhotonNetwork.IsMasterClient);
+        xrOrigin.SetActive(false);
+        PhotonNetwork.Instantiate(genericVRPlayerPrefab.name, spawnPosition, Quaternion.identity);
     }
 
     private void ShowPlayers() {
