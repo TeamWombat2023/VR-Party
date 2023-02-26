e3701f8836640c91427c6e0cd9e0bc9c2fe1b11e
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-02-26 18:40:52 +0300

    Fixed some functionalities

--- Assets/Code/Scripts/Manager/MultiplayerNetworkManager/NetworkManager.cs
+++ Assets/Code/Scripts/Manager/MultiplayerNetworkManager/NetworkManager.cs
@@ -7,35 +7,30 @@ public class NetworkManager : MonoBehaviourPunCallbacks {
     public TMP_Text playerNamesText;
     public TMP_Text lobbyInfoText;
     public GameObject startTheGameButton;
-    
-    [SerializeField]
-    private GameObject xrOrigin;
-    [SerializeField]
-    private GameObject genericVRPlayerPrefab;
-    [SerializeField]
-    private Vector3 spawnPosition;
+
+    [SerializeField] private GameObject xrOrigin;
+    [SerializeField] private GameObject genericVRPlayerPrefab;
+    [SerializeField] private Vector3 spawnPosition;
+    public Canvas lobbyCanvas;
+    public Canvas lobbyInfoCanvas;
 
     public override void OnJoinedRoom() {
         ShowPlayers();
         WriteLobbyInformation(PhotonNetwork.CurrentRoom);
         startTheGameButton.SetActive(PhotonNetwork.IsMasterClient);
         xrOrigin.SetActive(false);
-        genericVRPlayerPrefab = PhotonNetwork.Instantiate(genericVRPlayerPrefab.name, spawnPosition, Quaternion.identity);
+        PhotonNetwork.Instantiate(genericVRPlayerPrefab.name, spawnPosition, Quaternion.identity);
+        lobbyCanvas.worldCamera = genericVRPlayerPrefab.GetComponentInChildren<Camera>();
+        lobbyInfoCanvas.worldCamera = genericVRPlayerPrefab.GetComponentInChildren<Camera>();
     }
-    
+
     public override void OnJoinRoomFailed(short returnCode, string message) {
         PhotonNetwork.LoadLevel("Login Scene");
-    }
-    
-    public override void OnLeftRoom() {
-        PhotonNetwork.Destroy(genericVRPlayerPrefab);
     }
 
     private void ShowPlayers() {
         var players = "";
-        foreach (var player in PhotonNetwork.PlayerList) {
-            players += player.NickName + "\n";
-        }
+        foreach (var player in PhotonNetwork.PlayerList) players += player.NickName + "\n";
         playerNamesText.text = players;
     }
 
@@ -50,8 +45,8 @@ public class NetworkManager : MonoBehaviourPunCallbacks {
     }
 
     private void WriteLobbyInformation(Room room) {
-        lobbyInfoText.text = "Lobby Name: " + room.Name +"\n";
-        lobbyInfoText.text += "Players:" + room.PlayerCount + "/"+ room.MaxPlayers+ "\n";
+        lobbyInfoText.text = "Lobby Name: " + room.Name + "\n";
+        lobbyInfoText.text += "Players:" + room.PlayerCount + "/" + room.MaxPlayers + "\n";
     }
 
     public override void OnMasterClientSwitched(Player newMasterClient) {
