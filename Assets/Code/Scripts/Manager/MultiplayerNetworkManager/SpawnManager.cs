91a7f19055caf64a9b1a5c1b3e10d361fccf91fc
Selin Mergen <selinmergen@gmail.com>
2023-03-02 00:39:00 +0300

    Removed unnecessary parts

--- Assets/Code/Scripts/Manager/MultiplayerNetworkManager/SpawnManager.cs
+++ Assets/Code/Scripts/Manager/MultiplayerNetworkManager/SpawnManager.cs
@@ -1,32 +1,16 @@
 using Photon.Pun;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviourPunCallbacks {
+public class SpawnManager : MonoBehaviour {
     [SerializeField] private GameObject GenericVRPlayerPrefab;
 
     public Vector3 spawnPosition;
 
-    // private void Start() {
-    //     Invoke(nameof(SpawnPlayer), 0.5f);
-    // }
-    //
-    // private void SpawnPlayer() {
-    //     PhotonNetwork.Instantiate(GenericVRPlayerPrefab.name, spawnPosition, Quaternion.identity);
-    // }
-
     private void Start() {
-        PhotonNetwork.ConnectUsingSettings();
+        Invoke(nameof(SpawnPlayer), 0.5f);
     }
 
-    public override void OnConnectedToMaster() {
-        PhotonNetwork.JoinLobby();
-    }
-
-    public override void OnJoinedLobby() {
-        PhotonNetwork.JoinOrCreateRoom("Room", new Photon.Realtime.RoomOptions { MaxPlayers = 4 }, null);
-    }
-
-    public override void OnJoinedRoom() {
+    private void SpawnPlayer() {
         PhotonNetwork.Instantiate(GenericVRPlayerPrefab.name, spawnPosition, Quaternion.identity);
     }
 }
