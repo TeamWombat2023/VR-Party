b14ce29a2e253d6dbb8765cbfc99b22bd239b9af
MErenB <78208804+MErenB@users.noreply.github.com>
2023-03-04 17:19:42 +0300

    A demo scene created for flying tests.

--- Assets/Code/Scripts/Plane Game/PlaneGameManager.cs
+++ Assets/Code/Scripts/Plane Game/PlaneGameManager.cs
@@ -5,51 +5,16 @@ using UnityEngine;
 
 public class PlaneGameManager : MonoBehaviour
 {
-    [Header("Powerup respawn time")]
-    public int powerupRespawnTime;
-
-    
-    [Header("Trigger Holders")]
-    public GameObject checkPointsHolder;
-    public GameObject powerupHolder;
-
-    private Checkpoint[] _checkpoints;
-    private int _currentCheckpoint;
-    
-    private Powerup[] _powerups;
-    
-    
-    
+    public TMP_Text debugText;
     // Start is called before the first frame update
     void Start()
     {
-        _checkpoints= checkPointsHolder.GetComponentsInChildren<Checkpoint>();
-        _currentCheckpoint = 0;
         
-        _powerups = powerupHolder.GetComponentsInChildren<Powerup>();
-        EnableNewCheckpoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-
-    public void EnableNewCheckpoint()
-    {
-         foreach (var checkpoint in _checkpoints)
-         {
-             checkpoint.gameObject.SetActive(false); 
-         }
-
-         _checkpoints[_currentCheckpoint++].gameObject.SetActive(true);
-    }
-
-
-    public void StartPowerupRespawnTimer()
-    {
         
     }
-
 }
