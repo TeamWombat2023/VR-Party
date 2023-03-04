cf10abe1ad6ecd866c34ca7c1a4e3cbce5484172
MErenB <78208804+MErenB@users.noreply.github.com>
2023-03-04 17:19:42 +0300

    A demo scene created for flying tests.

---
+++ Assets/Code/Scripts/Plane Game/Pilot.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Pilot : MonoBehaviour
+{
+    public GameObject airplane;
+    public GameObject rightHand;
+    public TMP_Text debugText;
+
+    private ActionBasedController rightHandXRController;
+
+    private Quaternion startRotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rightHandXRController = rightHand.GetComponent<ActionBasedController>();
+       StartCoroutine(UpdateRotationText());
+       startRotation = rightHand.transform.rotation;
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //Debug.Log("Rotation of the right hand:"+ rightHand.transform.rotation);
+       GetHandInput();
+    }
+
+    private void GetHandInput()
+    {
+        var currentRotation = rightHand.transform.rotation;
+        Quaternion relative = Quaternion.Inverse(startRotation) * currentRotation;
+        debugText.text = relative.eulerAngles.ToString();
+        //Debug.Log(relative.eulerAngles.ToString());
+
+        airplane.GetComponent<Plane>().steeringInput = relative.eulerAngles;
+   
+        
+    }
+    
+    
+
+    public IEnumerator UpdateRotationText()
+    {
+        //I have relative rotation. Up for 360 degrees, down for 4-5 degrees.
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1);
+
+            var currentRotation = rightHand.transform.rotation;
+            Quaternion relative = Quaternion.Inverse(startRotation) * currentRotation;
+            debugText.text = " ";
+            debugText.text = relative.eulerAngles.ToString();
+
+            //airplane.GetComponent<Plane>().steeringInput = relative.eulerAngles;
+
+            // debugText.text += "Rotation in x: " + rightHand.transform.rotation.eulerAngles.x + "\n";
+            // debugText.text += "Rotation in y: " + rightHand.transform.rotation.eulerAngles.y + "\n";
+            // debugText.text += "Rotation in z: " + rightHand.transform.rotation.eulerAngles.z + "\n";
+        }
+        
+    }
+    
+    
+}
