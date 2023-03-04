1a02e4534eb9a6a17ef646773dfb9928f91e850d
MErenB <78208804+MErenB@users.noreply.github.com>
2023-03-04 17:19:42 +0300

    A demo scene created for flying tests.

---
+++ Assets/Code/Scripts/Plane Game/PlaneGameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlaneGameManager : MonoBehaviour
+{
+    public TMP_Text debugText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
