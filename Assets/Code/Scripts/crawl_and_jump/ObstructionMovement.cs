9da3c6da31a58b2c4579d0368326d8cb15eabc7a
kaanefekeles <kaanefek@gmail.com>
2023-03-06 18:25:00 +0300

    Obstruction generation, physics and lava

--- Assets/Code/Scripts/crawl_and_jump/ObstructionMovement.cs
+++ Assets/Code/Scripts/crawl_and_jump/ObstructionMovement.cs
@@ -6,7 +6,6 @@ public class ObstructionMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody rb;
-    public bool isRotating = false;
     void Start()
     {
         rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
@@ -15,15 +14,8 @@ public class ObstructionMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //transform.Translate(-Vector3.right * Time.deltaTime * 10);
+        transform.Translate(-Vector3.right * Time.deltaTime * 10);
         //rb.AddForce(2000 * Time.deltaTime, 0, 0);
-        rb.velocity = new Vector3(-10, 0, 0);
-        if(isRotating){
-            transform.Rotate(0f, 1f, 0f, Space.Self);
-        }
-
-        if(transform.position.x < -50){
-            Destroy(gameObject);
-        }
+        //1rb.velocity = new Vector3(10, 0, 0);
     }
 }
