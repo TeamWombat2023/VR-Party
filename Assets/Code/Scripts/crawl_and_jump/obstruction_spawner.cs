43325da18975a29c02ad196160d50f23a6930991
kaanefekeles <kaanefek@gmail.com>
2023-03-06 18:25:00 +0300

    Obstruction generation, physics and lava

--- Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
+++ Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class obstruction_spawner : MonoBehaviour
 {
     public GameObject obstructionPrefab;
+    public float random_number = 0f;
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)){
-            Instantiate(obstructionPrefab, transform.position, Quaternion.identity);
+            var clone = Instantiate(obstructionPrefab, transform.position, Quaternion.identity);
+            random_number = Random.Range(0,2);
+            if (random_number == 0)
+            {
+                clone.transform.position = clone.transform.position + new Vector3(0, 4, 0);
+            }
+            Debug.Log(clone.transform.position);
+            Debug.Log(random_number);
         }
     }
 }
