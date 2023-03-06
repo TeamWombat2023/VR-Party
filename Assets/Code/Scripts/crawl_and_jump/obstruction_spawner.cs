f5cabece46dd84774a56dbf584213a37cc949316
kaanefekeles <kaanefek@gmail.com>
2023-03-06 16:01:30 +0300

    Added the prefab for obstruction and initialized the random obstruction generation script

--- Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
+++ Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
@@ -1,37 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class obstruction_spawner : MonoBehaviour
 {
     public GameObject obstructionPrefab;
-    private float top_or_bottom = 0f;
     // Update is called once per frame
-    //void Update()
-    //{
-    //    if(Input.GetKeyDown(KeyCode.Space)){
-    //        var clone = Instantiate(obstructionPrefab, transform.position, Quaternion.identity);
-    //        top_or_bottom = Random.Range(0,2);
-    //        if(Random.Range(0,2) == 0){
-    //            clone.GetComponent<ObstructionMovement>().isRotating = true;
-    //        }
-    //        if (top_or_bottom == 0)
-    //        {
-    //            clone.transform.position = clone.transform.position + new Vector3(0, 3, 0);
-    //        }
-    //    }
-    //}
-
-    void Start(){
-        InvokeRepeating("obstruction_spawn", 5f, 3f);
-    }
-    void obstruction_spawn(){
-        var clone = Instantiate(obstructionPrefab, transform.position, Quaternion.identity);
-        top_or_bottom = Random.Range(0,2);
-        if(Random.Range(0,2) == 0){
-            clone.GetComponent<ObstructionMovement>().isRotating = true;
-        }
-        if (top_or_bottom == 0)
-        {
-            clone.transform.position = clone.transform.position + new Vector3(0, 1.75f, 0);
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Instantiate(obstructionPrefab, transform.position, Quaternion.identity);
         }
     }
 }
