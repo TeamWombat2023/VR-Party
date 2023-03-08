c02732397b87920c77f0c6e4e2007c01e0777df8
Lihem <mlhcsgn10@gmail.com>
2023-03-08 16:48:31 +0300

    Hand and gun movements sychronized.

--- Assets/PlayerSetup.cs
+++ Assets/PlayerSetup.cs
@@ -6,9 +6,13 @@ public class PlayerSetup : MonoBehaviour
 {
     public GameObject camera;
     public GameObject XRorg;
+    public GameObject leftHand;
+    public GameObject rightHand;
 
     public void IsLocalPlayer(){
         camera.SetActive(true);
         (XRorg.GetComponent("InputActionManager") as MonoBehaviour).enabled = true;
+        (leftHand.GetComponent("ActionBasedController") as MonoBehaviour).enabled = true;
+        (rightHand.GetComponent("ActionBasedController") as MonoBehaviour).enabled = true;
     } 
 }
