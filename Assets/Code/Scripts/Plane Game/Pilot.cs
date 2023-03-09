c0dc69f2ec471b4619f65ea4a2e71f5132cdef2b
MErenB <78208804+MErenB@users.noreply.github.com>
2023-03-09 19:14:47 +0300

    Flying physique added.

--- Assets/Code/Scripts/Plane Game/Pilot.cs
+++ Assets/Code/Scripts/Plane Game/Pilot.cs
@@ -6,53 +6,122 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Pilot : MonoBehaviour
 {
-    public GameObject airplane;
+    public Plane airplane;
     public GameObject rightHand;
+    public GameObject leftHand;
     public TMP_Text debugText;
 
-    private ActionBasedController rightHandXRController;
 
-    private Quaternion startRotation;
+    private bool isSteeringEnabled;
+    public AnimationCurve steeringXCurve;
+    public AnimationCurve steeringZCurve;
     
+    private bool isSpeedAdjustmentEnabled;
+    private float leftHandPositionBeginning;
+    public AnimationCurve thrustMappingCurve;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
         //rightHandXRController = rightHand.GetComponent<ActionBasedController>();
-       StartCoroutine(UpdateRotationText());
-       startRotation = rightHand.transform.rotation;
-       
+       isSteeringEnabled = false;
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
        //Debug.Log("Rotation of the right hand:"+ rightHand.transform.rotation);
-       GetHandInput();
+       if (isSteeringEnabled)
+       {
+           GetHandInput();
+       }
+
+       if (isSpeedAdjustmentEnabled)
+       {
+           SendThrustDifferenceToPlane();
+       }
+       
+    }
+    public void EnableSteering()
+    {
+        isSteeringEnabled = true;
+        debugText.text = "Grabbed";
+    }
+
+    public void DisableSteering()
+    {
+        isSteeringEnabled = false;
+        debugText.text = "Left";
+    }
+
+
+    public void EnableThrustModification()
+    {
+        isSpeedAdjustmentEnabled = true;
+        debugText.text = "thrust modification begun";
+        leftHandPositionBeginning = leftHand.transform.localPosition.z;
+    }
+    
+    
+    public void DisableThrustModification()
+    {
+        isSpeedAdjustmentEnabled = false;
+        debugText.text = "thrust modification end";
     }
 
     private void GetHandInput()
     {
-        var currentRotation = rightHand.transform.rotation;
-        //var defaultRotation = airplane.transform.forward + airplane.transform.right + airplane.transform.up;
-        
-        //Quaternion relative = Quaternion.Inverse(startRotation) * currentRotation;
+        var currentHandRotation = rightHand.transform.rotation;
         //difference between the plane and the right hand.
-        Quaternion relative = Quaternion.Inverse(airplane.transform.rotation) * currentRotation;
-        debugText.text = relative.eulerAngles.ToString();
-        //Debug.Log(relative.eulerAngles.ToString());
+        Quaternion relativeQuat = Quaternion.Inverse(airplane.transform.rotation) * currentHandRotation;
+        float relativeVecX = relativeQuat.eulerAngles.x < 180 ? relativeQuat.eulerAngles.x : relativeQuat.eulerAngles.x -360;
+        float relativeVecY = relativeQuat.eulerAngles.y < 180 ? relativeQuat.eulerAngles.y : relativeQuat.eulerAngles.y - 360;
+        float relativeVecZ = relativeQuat.eulerAngles.z < 180 ? relativeQuat.eulerAngles.z : relativeQuat.eulerAngles.z - 360;
 
-        airplane.GetComponent<Plane>().steeringInput = relative.eulerAngles;
-   
+
+        relativeVecX = steeringXCurve.Evaluate(relativeVecX);
+        relativeVecZ = steeringZCurve.Evaluate(relativeVecZ);
+
+
+        relativeVecX = -relativeVecX;
+        relativeVecZ = -relativeVecZ;
+        Vector3 relativeVec = new Vector3(relativeVecX, relativeVecY, relativeVecZ);
+
+        //Quaternion absoluteHandRotation = Quaternion.Inverse(relative) * currentRotation;
         
+        debugText.text = "Hand's rotation: " + currentHandRotation.eulerAngles.ToString() + "\n";
+        debugText.text += "RelativeVec:" + relativeVec + "\n";
+
+        airplane.steeringInput = relativeVec;
     }
 
-    private void PlacePilotToPlane()
+    private void SendThrustDifferenceToPlane()
     {
-    }
-    
-    
+        var currentLeftHandPosition = leftHand.transform.localPosition.z;
 
+        //var currentThrust = airplane.GetComponent<Plane>().thrust;
+
+        var difference = currentLeftHandPosition - leftHandPositionBeginning;
+
+        var newThrust = airplane.thrustInput + thrustMappingCurve.Evaluate(difference);
+        if (newThrust > 1)
+        {
+            newThrust = 1;
+        }
+        else if (newThrust < 0)
+        {
+            newThrust = 0;
+        }
+        
+        airplane.thrustInput = airplane.thrustInput + thrustMappingCurve.Evaluate(difference);
+    }
+
+
+
+    
+    
     public IEnumerator UpdateRotationText()
     {
         //I have relative rotation. Up for 360 degrees, down for 4-5 degrees.
@@ -61,9 +130,7 @@ public class Pilot : MonoBehaviour
             yield return new WaitForSecondsRealtime(1);
 
             var currentRotation = rightHand.transform.rotation;
-            Quaternion relative = Quaternion.Inverse(startRotation) * currentRotation;
             debugText.text = " ";
-            debugText.text = relative.eulerAngles.ToString();
 
             //airplane.GetComponent<Plane>().steeringInput = relative.eulerAngles;
 
