78954be3e1c863dc46d4841d57a19549c16c31da
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-03-13 00:26:50 +0300

    Added temporary buttons to load minigames

--- Assets/Code/Scripts/Minigame FPS/FPSSceneEnter.cs
+++ Assets/Code/Scripts/Minigame FPS/FPSSceneEnter.cs
@@ -1,16 +1,8 @@
-using TMPro;
 using Photon.Pun;
 using UnityEngine;
-using UnityEngine.UI;
-using Photon.Realtime;
-using System.Collections.Generic;
-using System.Linq;
-using ExitGames.Client.Photon;
 
-public class FPSSceneEnter : MonoBehaviourPunCallbacks
-{
-    void OnTriggerEnter(Collider other){
-        Debug.Log("asfasfasfsa");
-        PhotonNetwork.LoadLevel("Minigame FPS Scene");
+public class FPSSceneEnter : MonoBehaviourPunCallbacks {
+    private void OnTriggerEnter(Collider other) {
+        PhotonNetwork.LoadLevel("FPS Scene");
     }
 }
