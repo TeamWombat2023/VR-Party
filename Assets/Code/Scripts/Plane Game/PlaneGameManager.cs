3dcaf498ef6bfd639721ac5cb23a7382059700f0
MErenB <78208804+MErenB@users.noreply.github.com>
2023-03-12 20:52:05 +0300

    Checkpoint system added.

--- Assets/Code/Scripts/Plane Game/PlaneGameManager.cs
+++ Assets/Code/Scripts/Plane Game/PlaneGameManager.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 
 public class PlaneGameManager : MonoBehaviour
 {
-    public TMP_Text debugText;
+    public GameObject checkPointsHolder;
+
+    private Checkpoint[] _checkpoints;
+    private int _currentCheckpoint;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _checkpoints= checkPointsHolder.GetComponentsInChildren<Checkpoint>();
+        _currentCheckpoint = 0;
+        EnableNewCheckpoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    public void FireTheGun()
+
+    public void EnableNewCheckpoint()
     {
-        Debug.Log("Gun is fired.");
+         foreach (var checkpoint in _checkpoints)
+         {
+             checkpoint.gameObject.SetActive(false); 
+         }
+
+         _checkpoints[_currentCheckpoint++].gameObject.SetActive(true);
     }
+
 }
