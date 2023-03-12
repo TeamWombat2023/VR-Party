42cc954f78708942ea0a9cf88d959f71146ac04b
Selin Mergen <72672134+Morjin1062@users.noreply.github.com>
2023-03-12 23:29:16 +0300

    Fixed hand rotation bug

--- Assets/Code/Scripts/Fruit Ninja/ScoreManager.cs
+++ Assets/Code/Scripts/Fruit Ninja/ScoreManager.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour {
     private int _score;
     public TMP_Text scoreText;
+    private FruitSpawner _fruitSpawner;
+    public float gameDuration = 60f;
 
     private void Start() {
+        _fruitSpawner = FindObjectOfType<FruitSpawner>();
         scoreText.gameObject.SetActive(false);
+        Invoke(nameof(FinishGame), gameDuration);
     }
 
     public void IncrementScore() {
@@ -16,5 +20,11 @@ public class ScoreManager : MonoBehaviour {
     public void FinishGame() {
         scoreText.gameObject.SetActive(true);
         scoreText.text = "Score: " + _score;
+    }
+
+    public void GameOver() {
+        _fruitSpawner.gameObject.SetActive(false);
+        scoreText.gameObject.SetActive(true);
+        scoreText.text = "Game Over";
     }
 }
