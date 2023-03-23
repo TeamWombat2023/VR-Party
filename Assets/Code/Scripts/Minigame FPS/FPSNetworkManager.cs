1f4b76e4f13b8a6b690c610eeedb3b8688b45df4
Lihem <mlhcsgn10@gmail.com>
2023-03-23 14:20:59 +0300

    Respawn Camera and Respawning system added.

--- Assets/Code/Scripts/Minigame FPS/FPSNetworkManager.cs
+++ Assets/Code/Scripts/Minigame FPS/FPSNetworkManager.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 
 public class FPSNetworkManager : MonoBehaviourPunCallbacks {
 
+    public static FPSNetworkManager instance;
+
     [SerializeField] private GameObject fpsVRPlayerPrefab;
     [Space]
     [SerializeField] private Transform spawnPoint;
-    
+    [Space]
+    [SerializeField] public GameObject roomCam;
+
+    void Awake(){
+        instance = this;
+    }
+
     private void Start() {
         Debug.Log("JOINED MINIGAME");
-        GameObject _player = PhotonNetwork.Instantiate(fpsVRPlayerPrefab.name, spawnPoint.position, Quaternion.identity);
-        _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+        
+        roomCam.SetActive(false);
+        SpawnPlayerWithDelay();
     }
 
     public override void OnJoinRoomFailed(short returnCode, string message) {
@@ -25,6 +34,19 @@ public class FPSNetworkManager : MonoBehaviourPunCallbacks {
     
     public override void OnDisconnected(DisconnectCause cause) {
         PhotonNetwork.LoadLevel("Lobby Scene");
+    }
+
+
+    public void SpawnPlayerWithDelay(){
+        roomCam.SetActive(true);
+        Invoke("RespawnPlayer", 4);
+    }
+
+    public void RespawnPlayer(){
+        roomCam.SetActive(false);
+        GameObject _player = PhotonNetwork.Instantiate(fpsVRPlayerPrefab.name, spawnPoint.position, Quaternion.identity);
+        _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+        _player.GetComponent<FPSPlayerHealth>().isLocalPlayer = true;
     }
 
 }
--- Assets/Code/Scripts/Minigame FPS/FPSPlayerHealth.cs
+++ Assets/Code/Scripts/Minigame FPS/FPSPlayerHealth.cs
@@ -7,12 +7,17 @@ using Photon.Realtime;
 public class FPSPlayerHealth : MonoBehaviour
 {
     public int health;
+    public bool isLocalPlayer;
 
     [PunRPC]
     public void FPSDamageTake(int _damage){
         health -= _damage;
 
         if(health <= 0){
+
+            if(isLocalPlayer)
+                FPSNetworkManager.instance.SpawnPlayerWithDelay();
+
             Destroy(gameObject);
         }
     }
