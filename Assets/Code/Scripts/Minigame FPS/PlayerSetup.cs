bd9dad65fb51850a9668b024fb1dd2e318ffcd15
Selin Mergen <selinmergen@gmail.com>
2023-04-16 16:39:43 +0300

    Update PlayerSetup.cs

--- Assets/Code/Scripts/Minigame FPS/PlayerSetup.cs
+++ Assets/Code/Scripts/Minigame FPS/PlayerSetup.cs
@@ -1,24 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSetup : MonoBehaviour
-{
+public class PlayerSetup : MonoBehaviour {
     public GameObject camera;
     public GameObject XRorg;
     public GameObject leftHand;
     public GameObject rightHand;
 
-    public void IsLocalPlayer(){
+    public void IsLocalPlayer() {
         camera.SetActive(true);
         (XRorg.GetComponent("InputActionManager") as MonoBehaviour).enabled = true;
         //(XRorg.GetComponent("LocomotionSystem") as MonoBehaviour).enabled = true;
         (leftHand.GetComponent("ActionBasedController") as MonoBehaviour).enabled = true;
         (rightHand.GetComponent("ActionBasedController") as MonoBehaviour).enabled = true;
-        (rightHand.GetComponent("Weapon") as MonoBehaviour).enabled = false;
+        // (rightHand.GetComponent("Weapon") as MonoBehaviour).enabled = false;
     }
 
-    public void OpenWeapon(){
-        (rightHand.GetComponent("Weapon") as MonoBehaviour).enabled = true;
-    } 
+    public void OpenWeapon() {
+        // (rightHand.GetComponent("Weapon") as MonoBehaviour).enabled = true;
+    }
 }
