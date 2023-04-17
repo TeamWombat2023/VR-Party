b2bff60c227a49f31d6edae21b755767cc91a5ea
MErenB <78208804+MErenB@users.noreply.github.com>
2023-04-17 12:21:41 +0300

    Checkpoint ordering improved.

---
+++ Assets/Code/Scripts/Plane GameCode/CheckpointOrder.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Code.Scripts.Plane_GameCode
+{
+    public class CheckpointOrder : MonoBehaviour
+    {
+        
+        public Checkpoint[] Checkpoints;
+
+    }
+}
