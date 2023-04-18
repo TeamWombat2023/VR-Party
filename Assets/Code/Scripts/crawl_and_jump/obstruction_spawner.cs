6b0aa15867eaf4a8fef4e49386c90ae01e6e3505
Selin Mergen <selinmergen@gmail.com>
2023-04-18 17:16:01 +0300

    moved object under resources folder

--- Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
+++ Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using Photon.Pun;
-public class obstruction_spawner : MonoBehaviourPunCallbacks
-{
+
+public class obstruction_spawner : MonoBehaviourPunCallbacks {
     public GameObject obstructionPrefab;
+
     private float top_or_bottom = 0f;
     // Update is called once per frame
     //void Update()
@@ -20,20 +21,16 @@ public class obstruction_spawner : MonoBehaviourPunCallbacks
     //    }
     //}
 
-    void Start(){
+    private void Start() {
         InvokeRepeating("obstruction_spawn", 5f, 3f);
     }
-    void obstruction_spawn(){
-        if(PhotonNetwork.IsMasterClient){
-        var clone = Instantiate(obstructionPrefab, transform.position, Quaternion.identity);
-        top_or_bottom = Random.Range(0,2);
-        if(Random.Range(0,2) == 0){
-            clone.GetComponent<ObstructionMovement>().isRotating = true;
-        }
-        if (top_or_bottom == 0)
-        {
-            clone.transform.position = clone.transform.position + new Vector3(0, 1.75f, 0);
-        }
+
+    private void obstruction_spawn() {
+        if (PhotonNetwork.IsMasterClient) {
+            var clone = Instantiate(obstructionPrefab, transform.position, Quaternion.identity);
+            top_or_bottom = Random.Range(0, 2);
+            if (Random.Range(0, 2) == 0) clone.GetComponent<ObstructionMovement>().isRotating = true;
+            if (top_or_bottom == 0) clone.transform.position = clone.transform.position + new Vector3(0, 1.75f, 0);
         }
     }
 }
