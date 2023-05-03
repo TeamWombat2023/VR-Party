307844a438c2b673e73a477c3167648c7e95708e
okg21 <ogurbuz@etu.edu.tr>
2023-05-03 23:58:20 +0300

    added powerup for speed, for test

---
+++ Assets/rotator_powerup.cs
@@ -0,0 +1,29 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class rotator_powerup : MonoBehaviour {
+    private void Update() {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Head") || other.gameObject.CompareTag("Body"))
+            if (GameManager.gameManager.GetCurrentSceneName() == "Labyrinth Scene") {
+                if (PlayerManager.MasterClient != null) {
+                   if (PlayerManager.MasterClient.CustomProperties.ContainsKey("PickupCount")) {
+                        if ((int)PlayerManager.MasterClient.CustomProperties["PickupCount"] <= 0)
+                            LabyrinthNetworkManager.LabyrinthManager.FinishGame();
+                        else
+                            PlayerManager.MasterClient.SetCustomProperties(new ExitGames.Client.Photon.Hashtable {
+                                { "PickupCount", (int)PlayerManager.MasterClient.CustomProperties["PickupCount"] - 1 }
+                            });
+                }
+
+                if (other.transform.parent.parent == PlayerManager.LocalAvatar.transform &&
+                    LabyrinthNetworkManager.LabyrinthManager.GetTime() > 5)
+                    PlayerManager.LocalXROrigin.GetComponent<ActionBasedContinuousMoveProvider>().moveSpeed=10;
+                }
+        }
+    }
+
+}
