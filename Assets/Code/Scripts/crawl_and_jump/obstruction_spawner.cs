a1c5baddc5e04b2cb12fa7c650da9963e5cec0e2
Selin Mergen <selinmergen@gmail.com>
2023-05-03 22:20:46 +0300

    Visual Improvements Applied to Crawl and Jump scene

--- Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
+++ Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
@@ -5,13 +5,14 @@ public class obstruction_spawner : MonoBehaviourPunCallbacks {
     public GameObject obstructionPrefab;
 
     private float top_or_bottom = 0f;
+
     private void Start() {
         InvokeRepeating("obstruction_spawn", 5f, 3f);
     }
 
     private void obstruction_spawn() {
         if (PhotonNetwork.IsMasterClient) {
-            GameObject clone = PhotonNetwork.Instantiate("Obstruction", transform.position, Quaternion.identity);
+            var clone = PhotonNetwork.Instantiate("Obstruction", transform.position, Quaternion.Euler(90, 0, 0));
             top_or_bottom = Random.Range(0, 2);
             if (Random.Range(0, 2) == 0) clone.GetComponent<ObstructionMovement>().isRotating = true;
             if (top_or_bottom == 0) clone.transform.position = clone.transform.position + new Vector3(0, 0.8f, 0);
