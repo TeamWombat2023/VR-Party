1703199b5aa7a9bb61d132d89dbe1438d56613dc
kaanefekeles <kaanefek@gmail.com>
2023-05-04 05:25:59 +0300

    Crawling polished

--- Assets/Code/Scripts/crawl_and_jump/CrawlAndJumpInputManager.cs
+++ Assets/Code/Scripts/crawl_and_jump/CrawlAndJumpInputManager.cs
@@ -8,13 +8,17 @@ public class CrawlAndJumpInputManager : MonoBehaviour {
     private GameObject local_player;
     private Rigidbody _rigidbody;
 
+    private MeshCollider local_player_mesh_collider;
+
     private bool isGrounded;
     private bool isCrawling;
 
     private void Start() {
         local_player = PlayerManager.LocalPlayerInstance;
         isGrounded = true;
+        isCrawling = false;
         _rigidbody = local_player.GetComponent<Rigidbody>();
+        local_player_mesh_collider = local_player.transform.GetChild(1).transform.GetChild(1).transform.GetChild(1).GetComponent<MeshCollider>();
     }
 
     private void OnCollisionStay(Collision target) {
@@ -42,16 +46,23 @@ public class CrawlAndJumpInputManager : MonoBehaviour {
     }
 
     private void Jump(InputAction.CallbackContext obj) {
-        _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        Debug.Log("i be jumpin");
+        if(isGrounded){
+            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
         isGrounded = false;
     }
 
     private void Crawling(InputAction.CallbackContext obj) {
+        Debug.Log("i be crawlin");
         if (isCrawling) {
-            // local_player.transform.localScale = new Vector3(1, 1, 1);
-            // playeri kaldir
+            local_player.transform.position = local_player.transform.position + new Vector3(0, 1.3f, 0);
+            local_player_mesh_collider.enabled = true;
+            isCrawling = false;
         }
         else {
+            local_player_mesh_collider.enabled = false;
+            isCrawling = true;
             // local_player.transform.localScale = new Vector3(1, 0.5f, 1);
             // playeri yere indir
         }
