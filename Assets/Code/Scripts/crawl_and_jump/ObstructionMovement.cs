9ceeb2a07b6c4fa0e18a9b1bb2cae187827c3152
Selin Mergen <selinmergen@gmail.com>
2023-05-04 08:17:01 +0300

    Added fasten up obsticles in crawl and jump

--- Assets/Code/Scripts/crawl_and_jump/ObstructionMovement.cs
+++ Assets/Code/Scripts/crawl_and_jump/ObstructionMovement.cs
@@ -4,16 +4,19 @@ using Photon.Pun;
 public class ObstructionMovement : MonoBehaviour {
     // Start is called before the first frame update
     public Rigidbody rb;
-    public bool isRotating = false;
+    public int isRotating = -1;
+    public int speed = 7;
 
     private void Start() {
-        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ |
+                         RigidbodyConstraints.FreezeRotationY;
     }
 
     private void FixedUpdate() {
         if (PhotonNetwork.IsMasterClient) {
-            rb.velocity = new Vector3(-7, 0, 0);
-            if (isRotating) transform.Rotate(0f, 0f, 1f, Space.Self);
+            rb.velocity = new Vector3(-speed, 0, 0);
+            if (isRotating < 0) transform.Rotate(0f, 0f, 1f, Space.Self);
+            if (isRotating > 0) transform.Rotate(0f, 0f, -1f, Space.Self);
             if (transform.position.x < -50) PhotonNetwork.Destroy(gameObject);
         }
     }
--- Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
+++ Assets/Code/Scripts/crawl_and_jump/obstruction_spawner.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 using Photon.Pun;
 
 public class obstruction_spawner : MonoBehaviourPunCallbacks {
-    private float top_or_bottom = 0f;
+    private double _obstructionSpawnTimer = 0f;
 
     private void Start() {
         InvokeRepeating("obstruction_spawn", 10f, 3f);
+        _obstructionSpawnTimer = PhotonNetwork.Time;
     }
 
     private void obstruction_spawn() {
@@ -20,9 +21,12 @@ public class obstruction_spawner : MonoBehaviourPunCallbacks {
                     PhotonNetwork.PlayerList.Length) {
                     var clone = PhotonNetwork.Instantiate("Obstruction", transform.position,
                         Quaternion.Euler(90, 0, 0));
-                    top_or_bottom = Random.Range(0, 2);
-                    if (Random.Range(0, 2) == 0) clone.GetComponent<ObstructionMovement>().isRotating = true;
-                    if (top_or_bottom == 0) clone.transform.position += new Vector3(0, 0.8f, 0);
+                    var topOrBottom = Random.Range(0, 2);
+                    var randomRotation = Random.Range(0, 3);
+                    clone.GetComponent<ObstructionMovement>().isRotating = randomRotation;
+                    clone.GetComponent<ObstructionMovement>().speed =
+                        7 + (int)(PhotonNetwork.Time - _obstructionSpawnTimer) / 20;
+                    if (topOrBottom == 0) clone.transform.position += new Vector3(0, 0.8f, 0);
                 }
             }
         }
