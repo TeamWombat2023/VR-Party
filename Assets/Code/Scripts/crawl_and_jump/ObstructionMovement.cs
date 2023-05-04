2c724cc213e777034ee6699cc8a78dc3a5450412
kaanefekeles <kaanefek@gmail.com>
2023-05-04 03:59:15 +0300

    Final polish

--- Assets/Code/Scripts/crawl_and_jump/ObstructionMovement.cs
+++ Assets/Code/Scripts/crawl_and_jump/ObstructionMovement.cs
@@ -7,14 +7,13 @@ public class ObstructionMovement : MonoBehaviour {
     public bool isRotating = false;
 
     private void Start() {
-        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ |
-                         RigidbodyConstraints.FreezeRotationY;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
     }
 
     private void FixedUpdate() {
         if (PhotonNetwork.IsMasterClient) {
             rb.velocity = new Vector3(-7, 0, 0);
-            if (isRotating) transform.Rotate(0f, 1f, 0f, Space.Self);
+            if (isRotating) transform.Rotate(0f, 0f, 1f, Space.Self);
             if (transform.position.x < -50) PhotonNetwork.Destroy(gameObject);
         }
     }
